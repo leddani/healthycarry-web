19b5a0756d438fb31a39df4ed76de3bddf950c5d
leddani <129776616+leddani@users.noreply.github.com>
2025-03-29 14:39:51 +0100

    Update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+  experimental: {
+    appDir: true
+  }
 };
 
 export default nextConfig;
